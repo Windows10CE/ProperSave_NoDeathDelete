4957a1cd4d23d56b1ab4add0b0e1e871a2f45919
Aaron Robinson <aaronrobin1234@gmail.com>
2022-06-29 00:21:06 -0500

    Initial commit

---
+++ NoDeathDeletePlugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace ProperSave_NoDeathDelete;
+
+[BepInPlugin(ModGUID, ModName, ModVer)]
+[HarmonyPatch]
+public class NoDeathDeletePlugin : BaseUnityPlugin
+{
+    public const string ModGUID = "com.Windows10CE.ProperSave_NoDeathDelete";
+    public const string ModName = "ProperSave_NoDeathDelete";
+    public const string ModVer = "1.0.0";
+
+    private readonly Harmony HarmonyInstance = new(ModGUID);
+    
+    private void OnEnable()
+    {
+        HarmonyInstance.PatchAll(typeof(NoDeathDeletePlugin));
+    }
+
+    private void OnDisable()
+    {
+        HarmonyInstance.UnpatchSelf();
+    }
+
+    [HarmonyTargetMethod]
+    private static MethodInfo GetProperSaveDeleteMethod() =>
+        AccessTools.DeclaredMethod(typeof(ProperSave.ProperSavePlugin).Assembly.GetType("ProperSave.Saving"), "RunOnServerGameOver");
+
+    [HarmonyPrefix]
+    [HarmonyPatch]
+    public static bool DontRun() => false;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Windows10CE-ProperSave_NoDeathDelete
+
+Disables ProperSave deleting saves on death
+
+thats it
+thats the whole mod
